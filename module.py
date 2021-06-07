908b3b3941b22ba9799612aff5811ebadd25416d
Taregh Naderi <taregh.n@gmail.com>
2021-06-07 22:29:51 +0430

    coding add_wp and del_wp funcs

--- module.py
+++ module.py
@@ -1,6 +1,5 @@
 import sqlite3
 import inquirer
-from inquirer import questions
 
 class Nurse:
     def __init__(self, name, family, nurse_id):
@@ -8,22 +7,37 @@ class Nurse:
         self.family = family
         self.nurse_id = nurse_id
 
-    def add_workplace(self, city, hospital, ward):
+    def add_workplace(self, workplace = True):
+        questions = insert_data(workplace)
+        answers = inquirer.prompt(questions)
+        city = answers['city']
+        hospital = answers['hospital']
+        ward = answers['ward']
         execute('INSERT INTO Workplaces VALUES (%s, %s, %s)' % (city, hospital, ward))
 
-    def del_workplace(self):
-        pass
+    def del_workplace(self, city):
+        wp_list = execute('SELECT * FROM Workplaces WHERE city = %s' % city)
+        questions = inquirer.List(
+            'Workplaces',
+            message= 'Select a Workplace: ',
+            choices= wp_list
+        )
+        answers = inquirer.prompt(questions)
+        city = answers['Workplaces'][0]
+        hospital = answers['Workplaces'][1]
+        ward = answers['Workplaces'][2]
+        execute('DELETE FROM Workplaces WHERE city = %s AND hospital = %s AND ward = %s)' 
+        % (city, hospital, ward))
 
-    def add_shift(self, workplase = False, date = True, shift = True):
-        select_wp = select_data()
+    def add_shift(self, workplace = True, date = True, shift = True):
+        select_wp = select_data(workplace)
         wp = inquirer.prompt(select_wp)
-        if wp != '* New workplase':
-            city = workplase['workplase'][0]
-            hospital = workplase['workplase'][1]
-            ward = workplase['workplase'][2]
-        else:
-            workplase = True
-        questions = insert_data(workplase, date, shift)
+        if wp != '* New workplace':
+            city = workplace['workplace'][0]
+            hospital = workplace['workplace'][1]
+            ward = workplace['workplace'][2]
+            workplace = False
+        questions = insert_data(workplace, date, shift)
         answers = inquirer.prompt(questions)
         if 'city' and 'hospital' and 'ward' in answers.keys():
             city = answers['city']
@@ -56,13 +70,13 @@ class Nurse:
         % (city, hospital, ward, year, month, day, shift, holiday)
         )
 
-
     def edit_shift(self):
         pass
 
     def workplaces_list(self):
         workplaces = execute('SELECT * FROM Workplace')
-        return workplaces
+        for wp in workplaces:
+            print(wp)
 
     def select_month(self):
         pass
@@ -76,9 +90,9 @@ def execute(text):
     cnx.close()
     return content
 
-def insert_data(workplase = False, date = False, shift = False):
+def insert_data(workplace = False, date = False, shift = False):
     questions = []
-    if workplase:
+    if workplace:
         questions.append(
             inquirer.Text("city", message="City: "),
             inquirer.Text("hospital", message="Hospital name: "),
@@ -105,16 +119,16 @@ def insert_data(workplase = False, date = False, shift = False):
         )
     return questions
 
-def select_data(workplase = False, date = False):
+def select_data(workplace = False, date = False):
     questions = []
-    if workplase:
-        workplases = execute('SELECT * FROM Workplaces')
-        workplases.append('* New workplase')
+    if workplace:
+        workplaces = execute('SELECT * FROM Workplaces')
+        workplaces.append('* New workplace')
         questions.append(
             inquirer.List(
-                'workplase',
-                message= "Select workplase: ",
-                choices= workplases
+                'workplace',
+                message= "Select workplace: ",
+                choices= workplaces
             )
         )
     if date:
@@ -145,7 +159,7 @@ def help():
 - add           Add new shift to database
 - del           Del a shift from database
 - edit          Edit a shift from database
-- wp            See list og workplases
-- add wp        define new workplase
+- wp            See list og workplaces
+- add wp        define new workplace
 - month         Select an show shifts of a month
 ------------------------------------------------------''')
