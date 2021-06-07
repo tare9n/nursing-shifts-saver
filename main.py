38d2083d7587cab18dcb6e94cf35b76d6edfed2d
Taregh Naderi <taregh.n@gmail.com>
2021-06-07 15:05:31 +0430

    add input_data func del shift_id and some other

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from module import *
 
 execute('''CREATE TABLE IF NOT EXISTS Shifts 
-        (id TEXT NOT NULL PRIMARY KEY, hospital TEXT, unit TEXT, year NUMBER, month NUMBER, day NUMBER, shift TEXT, holiday TEXT)''')
+        (hospital TEXT, unit TEXT, year NUMBER, month NUMBER, day NUMBER, shift TEXT, holiday TEXT)''')
 
 execute('''CREATE TABLE IF NOT EXISTS Workplaces 
         (city TEXT, hospital TEXT, unit TEXT)''')
--- module.py
+++ module.py
@@ -1,4 +1,6 @@
 import sqlite3
+import inquirer
+from inquirer import questions
 
 class Nurse:
     def __init__(self, name, family, nurse_id):
@@ -9,15 +11,12 @@ class Nurse:
     def add_workplace(self, city, hospital, ward):
         execute('INSERT INTO Workplaces VALUES (%s, %s, %s)' % (city, hospital, ward))
 
-    def make_shift_id(self):
-        pass
-
-    def add_shift(self, id, city, hospital, ward, year, month, day, shift, holiday = 'No'):
+    def add_shift(self, city, hospital, ward, year, month, day, shift, holiday = 'No'):
         execute(f'INSERT INTO Shifts VALUES (%s, %s, %s, %i, %i, %i, %s, %s)'
-         %(id, city, hospital, ward, year, month, day, shift, holiday))
+         %(city, hospital, ward, year, month, day, shift, holiday))
 
-    def del_shift(self):
-        pass
+    def del_shift(self, year, month, day):
+        execute(f'SELECT * FROM Shifts WHERE year = %i AND month = %i AND day = %i' % (year, month, day))
 
     def edit_shift(self):
         pass
@@ -50,3 +49,31 @@ def execute(text):
     cnx.commit()
     cnx.close()
     return content
+
+def input_data(workplase = False, date = False, shift = False):
+    questions = []
+    if workplase:
+        workplases = execute('SELECT * FROM Workplaces')
+        workplases.append('* New workplase')
+        questions.append(
+            inquirer.List(
+                'workplase',
+                message= "Select workplase: ",
+                choices= workplases
+            )
+        )
+    if date:
+        questions.append(
+            inquirer.Text("year", message="year: "),
+            inquirer.Text("month", message="month: "),
+            inquirer.Text("day", message="day: "),
+            )
+    if shift:
+        questions.append(
+            inquirer.Checkbox(
+                'shift',
+                message= 'Check shift(s): ',
+                choices= ['M', 'E', 'N']
+            )
+        )
+    return questions
