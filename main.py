b69164915b6254049cf0a7212c8083e45170aba1
Taregh Naderi <taregh.n@gmail.com>
2021-06-07 17:55:27 +0430

    change add_shift func

--- main.py
+++ main.py
@@ -5,10 +5,3 @@ execute('''CREATE TABLE IF NOT EXISTS Shifts
 
 execute('''CREATE TABLE IF NOT EXISTS Workplaces 
         (city TEXT, hospital TEXT, unit TEXT)''')
-
-
-
-# while True:
-#     while True:
-#         request = input('> ')
-#         if request in ('add', 'del', 'brief', )
--- module.py
+++ module.py
@@ -11,7 +11,27 @@ class Nurse:
     def add_workplace(self, city, hospital, ward):
         execute('INSERT INTO Workplaces VALUES (%s, %s, %s)' % (city, hospital, ward))
 
-    def add_shift(self, city, hospital, ward, year, month, day, shift, holiday = 'No'):
+    def add_shift(self, workplase = False, date = True, shift = True):
+        select_wp = select_data()
+        workplase = inquirer.prompt(select_wp)
+        if workplase != '* New workplase':
+            city = workplase['workplase'][0]
+            hospital = workplase['workplase'][1]
+            ward = workplase['workplase'][2]
+            workplase = False
+        else:
+            workplase = True
+        questions = insert_data(workplase, date, shift)
+        answers = inquirer.prompt(questions)
+        if city in answers.keys():
+            city = answers['city']
+            hospital = answers['hospital']
+            ward = answers['ward']
+        year = answers['year']
+        month = answers['month']
+        day = answers['day']
+        shift = answers['shift']
+        holiday = answers['holiday']
         execute(f'INSERT INTO Shifts VALUES (%s, %s, %s, %i, %i, %i, %s, %s)'
          %(city, hospital, ward, year, month, day, shift, holiday))
 
@@ -28,19 +48,6 @@ class Nurse:
     def select_month(self):
         pass
 
-    def help(self):
-        print('''------------------------------------------------------
-                            Help
-    ------------------------------------------------------
-    - brief         See brief of your shift records
-    - add           add
-    - del           del
-    - edit          edit
-    - wards         wards
-    - month         select
-    ------------------------------------------------------''')
-
-
 def execute(text):
     cnx = sqlite3.connect('./data.db')
     cursor = cnx.cursor()
@@ -50,30 +57,57 @@ def execute(text):
     cnx.close()
     return content
 
-def input_data(workplase = False, date = False, shift = False):
+def insert_data(workplase = False, date = False, shift = False):
     questions = []
     if workplase:
-        workplases = execute('SELECT * FROM Workplaces')
-        workplases.append('* New workplase')
         questions.append(
-            inquirer.List(
-                'workplase',
-                message= "Select workplase: ",
-                choices= workplases
-            )
+            inquirer.Text("city", message="City: "),
+            inquirer.Text("hospital", message="Hospital name: "),
+            inquirer.Text("ward", message="Ward: "),
         )
     if date:
         questions.append(
             inquirer.Text("year", message="year: "),
             inquirer.Text("month", message="month: "),
             inquirer.Text("day", message="day: "),
-            )
+        )
     if shift:
         questions.append(
             inquirer.Checkbox(
                 'shift',
                 message= 'Check shift(s): ',
                 choices= ['M', 'E', 'N']
+            ),
+            inquirer.List(
+                "holiday",
+                message="Is it a holiday?",
+                choices= ['Yes', 'No']
             )
         )
     return questions
+
+def select_data():
+    questions = []
+    workplases = execute('SELECT * FROM Workplaces')
+    workplases.append('* New workplase')
+    questions.append(
+        inquirer.List(
+            'workplase',
+            message= "Select workplase: ",
+            choices= workplases
+        )
+    )
+    return questions
+
+def help():
+    print('''------------------------------------------------------
+                         Help
+------------------------------------------------------
+- brief         See brief of your shift records
+- add           Add new shift to database
+- del           Del a shift from database
+- edit          Edit a shift from database
+- wp            See list og workplases
+- add wp        define new workplase
+- month         Select an show shifts of a month
+------------------------------------------------------''')
